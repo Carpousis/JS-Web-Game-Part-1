a7f407090420dc5f7a161f53c59c320e34021574
Christopher Carpousis <bruceleeonidas@christophersair.myfiosgateway.com>
2022-05-10 12:33:00 -0400

    part one complete

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const newItem = (url, x, y) => {
         object.remove()
     })
 }
-newImage ('')
+newImage('assets/pine-tree.png', 450, 200)
 newImage('assets/green-character.gif', 100, 100)   
 newImage('assets/tree.png', 200, 300)
 newImage('assets/pillar.png', 350, 100)
